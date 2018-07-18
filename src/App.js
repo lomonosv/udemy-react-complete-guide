8d550aa35000c3521240a1333d1c4c19ecc01429
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-18 11:11:43 +0300

    Clean up

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Person from './Person/Person';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   state = {
@@ -12,11 +10,7 @@ class App extends Component {
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other state',
-    usernames: [
-      'Max',
-      'Manu'
-    ]
+    otherState: 'some other state'
   };
 
   switchNameHandler = (newName) => {
@@ -39,15 +33,6 @@ class App extends Component {
     });
   };
 
-  usernameChangeHandler = (event) => {
-    this.setState({
-      usernames: [
-        event.target.value,
-        'Manu'
-      ]
-    });
-  };
-
   render() {
     const style = {
       backgroundColor: 'white',
@@ -61,10 +46,6 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-
-        <UserInput changed={ this.usernameChangeHandler } username={ this.state.usernames[0] }/>
-        <UserOutput username={ this.state.usernames[0] }/>
-        <UserOutput username={ this.state.usernames[1] }/>
 
         <button
           style={ style }
