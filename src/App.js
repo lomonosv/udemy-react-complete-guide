83645b0f5de0aa7f959ecfa2f1f0eeab29f02d16
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-19 09:06:00 +0300

    Added name changed handler for list items

--- src/App.js
+++ src/App.js
@@ -14,14 +14,17 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Vladimir', age: 30 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    });
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   };
 
   deletePersonHandler = (personIndex) => {
@@ -60,7 +63,8 @@ class App extends Component {
                   click={ this.deletePersonHandler.bind(this, index) }
                   name={ person.name }
                   age={ person.age }
-                  key={ person.id }/>
+                  key={ person.id }
+                  changed={ event => this.nameChangedHandler(event, person.id) }/>
             }) }
           </div>
         );
