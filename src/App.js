4b3bc513020e0c308cd474e199f4734531a71e65
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-20 08:44:59 +0300

    Radium package and hover pseudo selector using

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 
 import Person from './Person/Person';
@@ -51,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -70,6 +75,10 @@ class App extends Component {
           </div>
         );
         style.backgroundColor = 'red';
+        style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+        };
     }
 
     let classes = [];
@@ -94,4 +103,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
