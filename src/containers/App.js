0be80f76e952e1d3a181ee1cf38e4120ccb7258f
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-25 08:40:03 +0300

    Better approach of using setState

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,8 @@ class App extends PureComponent {
         { id: 'asdf1', name: 'Stephanie', age: 26 }
       ],
       otherState: 'some other state',
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
   }
 
@@ -68,8 +69,11 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({
-      showPersons: !doesShow
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      };
     });
   };
 
