b55e1b167ba96d00b46f66743e0a1efd0c426735
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-25 10:10:17 +0300

    Using React Ref API

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,6 +9,7 @@ class Person extends Component {
   constructor(props) {
     super(props);
     console.log('[Person.js] Inside Constructor', props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -18,9 +19,14 @@ class Person extends Component {
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
     if (this.props.position === 0) {
-      this.inputElement.focus();
+      this.inputElement.current.focus();
     }
   }
+
+  focus() {
+    this.inputElement.current.focus();
+  }
+
   render() {
     console.log('[Person.js] Inside render()');
     return (
@@ -28,7 +34,7 @@ class Person extends Component {
         <p onClick={ this.props.click }>I'm a { this.props.name } and I am { this.props.age } years old!</p>
         <p>{ this.props.children }</p>
         <input
-          ref={ (inp) => {this.inputElement = inp;} }
+          ref={ this.inputElement }
           type="text"
           onChange={ this.props.changed }
           value={ this.props.name }/>
